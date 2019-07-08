de6f3a3d4f18f5e393dcb30c25a458b9a6da940e
Nikolay <SiUnit1996@gmail.com>
2019-07-08 10:36:31 +0700

    Страница result
    Поменял в маршруте путь test/result

---
+++ resources/views/result.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <style>
+        header{
+            text-align:center;
+        }
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Результаты теста</title>
+</head>
+
+<body>
+    <header>
+        <h1>Результаты тестов</h1>
+    </header>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('test','TestController@index')->name('test.index');;
+Route::get('test/result','TestController@index')->name('test.index');;
